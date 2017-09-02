42a800462e49f8a9e4ff2385dd5530dd63aed980
Andrey Piterov <pfm@live.ru>
2017-09-02 11:30:56 +0300

    Initial Commit

---
+++ Jack The GiantTests/Jack_The_GiantTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jack_The_GiantTests.swift
+//  Jack The GiantTests
+//
+//  Created by Andrew Piterov on 02/09/2017.
+//  Copyright © 2017 Andrew Pierov. All rights reserved.
+//
+
+import XCTest
+@testable import Jack_The_Giant
+
+class Jack_The_GiantTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
